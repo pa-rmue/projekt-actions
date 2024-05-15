95108ed3e1986caeff385665fd7e0e356d54cd4b
Robert Mueller <robert.mueller@plato.de>
2024-05-15 09:42:24 +0200

    timeouts and quarantine

--- .github/workflows/push-tests.js
+++ .github/workflows/push-tests.js
@@ -4,5 +4,8 @@ fixture`push-tests`
     .page`http://localhost:4200`;
 
 test('congrats exists', async t => {
-    await t.expect(Selector('p').withText('Congratulations!').exists).ok();
+    await t.expect(Selector('p', { timeout: 10000 }).withText('Congratulations!').exists).ok();
+}).timeouts({
+    pageLoadTimeout: 100000,
+    pageRequestTimeout : 100000,
 });
